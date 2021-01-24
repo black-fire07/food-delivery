395c5aeb598bb16755938329af61a7ea69f73297
black-fire07 <deepeshrawat77@gmail.com>
2021-01-24 19:06:21 +0530

    2nd

--- JS/style.js
+++ JS/style.js
@@ -1,31 +1,60 @@
-const body = document.body;
-const slides = document.querySelectorAll(".slide");
-const leftButton = document.getElementById("left");
-const rightButton = document.getElementById("right");
+let slides = document.querySelectorAll(".slide");
+let indicator = document.querySelectorAll(".indicator");
+let prev = document.querySelector(".prev");
+let next = document.querySelector(".next");
 
-let activeSlide = 0;
+let index = 0;
 
-const setBackground = () => {
-  body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
-};
+for (let i = 0; i < slides.length; i++) {
+  if (slides[i].classList.contains("active")) {
+    index = i
+  }
+}
 
-const setActiveSlide = () => {
-  slides.forEach((slide) => slide.classList.remove("active"));
-  slides[activeSlide].classList.add("active");
-};
+prev.onclick = prevSlide;
+next.onclick = nextSlide;
 
-rightButton.addEventListener("click", () => {
-  activeSlide++;
-  if (activeSlide > slides.length - 1) activeSlide = 0;
-  setBackground();
-  setActiveSlide();
+indicator.forEach(x => {
+  x.onclick = display;
 });
 
-leftButton.addEventListener("click", () => {
-  activeSlide--;
-  if (activeSlide < 0) activeSlide = slides.length - 1;
-  setBackground();
-  setActiveSlide();
-});
+function display() {
+  let num;
+  for (let i = 0; i < indicator.length; i++) {
+    indicator[i].classList.remove("active");
+    slides[i].classList.remove("active");
+  }
+  this.classList.add("active");
+  for (let i =0; i < indicator.length; i++) {
+    if (indicator[i].classList.contains("active")) {
+     num = i;
+    }
+  }
+  slides[num].classList.add("active");
+  index = num;
+}
 
-setBackground();
+function prevSlide() {
+  slides[index].classList.remove("active");
+  indicator[index].classList.remove("active");
+  index--;
+  console.log(index);
+  if (index < 0) {
+    index = slides.length - 1;
+  }
+  slides[index].classList.add("active");
+  indicator[index].classList.add("active");
+}
+
+function nextSlide() {
+  slides[index].classList.remove("active");
+  indicator[index].classList.remove("active");
+  index++;
+  console.log(index);
+  if (index == slides.length) {
+    index = 0;
+  }
+  console.log(index);
+  slides[index].classList.add("active");
+  indicator[index].classList.add("active");
+}
